ef26a24c1689f40f211d452d3dd6f6ea09dffafd
Matthew Lindly <matthew.lindly@hotmail.com>
2016-12-21 13:59:08 -0700

    Added migrations.

--- migrations/20161221133709_books_authors.js
+++ migrations/20161221133709_books_authors.js
@@ -1,8 +1,13 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return knex.schema.makeTable('books_authors', table => {
+    table.increments();
+    table.integer('book_id').notNullable().references('id').inTable('books');
+    table.integer('author_id').notNullable().references('id').inTable('authors');
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('books_authors');
 };
