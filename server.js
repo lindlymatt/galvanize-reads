20c7bd5152f45eca0d975d03e8e1a74b76ff8e09
Matthew Lindly <matthew.lindly@hotmail.com>
2016-12-21 14:53:33 -0700

    Added public for styling and JS needs.

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var bodyParser = require('body-parser');
+// Use the body parser module to allow xml responses.
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Set the view engine to PUG.
 app.set('view engine', 'pug')
 
-app.use()
+// Create variables for routes handling.
+const index = require('routes/index');
+const books = require('routes/books');
+const authors = require('routes/authors');
+
+// Application level middleware to send correct requests to places.
+app.get('/index', index);
+app.use('/books', books);
+app.use('/authors', authors);
+
+// Error handling, application-level middleware.
+app.use((err, _req, res, _next) => {
+  console.log(err);
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message);
+  }
+
+  if (err.status) {
+    console.log(err);
+    return res.status(err.status).send(err);
+  }
+
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+app.listen(8000, () => {
+  console.log('Currently listening for requests on Port 8000...')
+});
+
+module.exports = app;
